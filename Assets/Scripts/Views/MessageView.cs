c25ffbdec39f551d8a1614c1c197c151c8d8a8b1
tyutyunykh.a <artem.tyutyunykh@gmail.com>
2026-03-06 01:28:43 +0500

    [Add] Added main screen UI. Added views

---
+++ Assets/Scripts/Views/MessageView.cs
@@ -0,0 +1,32 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace TestGame.Views
+{
+    /// <summary>
+    /// View полосы сообщений.
+    /// </summary>
+    public class MessageView : MonoBehaviour
+    {
+        [SerializeField] private CanvasGroup _canvasGroup;
+        [SerializeField] private TextMeshProUGUI _messageText;
+
+        public CanvasGroup CanvasGroup => _canvasGroup;
+        public TextMeshProUGUI MessageText => _messageText;
+
+        public void SetText(string text)
+        {
+            _messageText.text = text;
+        }
+
+        public void Show()
+        {
+            _canvasGroup.alpha = 1f;
+        }
+
+        public void Hide()
+        {
+            _canvasGroup.alpha = 0f;
+        }
+    }
+}
---
+++ Assets/Scripts/Views/ScrollAreaView.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace TestGame.Views
+{
+    /// <summary>
+    /// View скролла с кубами.
+    /// </summary>
+    public class ScrollAreaView : MonoBehaviour
+    {
+        [SerializeField] private ScrollRect _scrollRect;
+        [SerializeField] private RectTransform _content;
+
+        public ScrollRect ScrollRect => _scrollRect;
+        public RectTransform Content => _content;
+    }
+}
